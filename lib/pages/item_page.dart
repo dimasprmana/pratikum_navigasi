5a97af5a449c503699c38315e92cc799b43c1f94
dimasprmana <dimasprmana>
2024-10-07 14:24:25 +0700

    dimascommit

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -4,15 +4,27 @@ import '../models/item.dart';
 class ItemPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Item item = ModalRoute.of(context)?.settings.arguments as Item;
+    // Ambil argumen item yang diteruskan dari HomePage
+    final Item item = ModalRoute.of(context)!.settings.arguments as Item;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(item.name),
-      ),
-      body: Center(
-        child: Text('Price: \$${item.price}'),
-      ),
-    );
-  }
+        appBar: AppBar(
+          title: Text(item.name),
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: item.name,
+                  child: Image.network(item.imageUrl), // Gambar item
+                ),
+                Text('Price: Rp ${item.price}'),
+                Text('Stock: ${item.stock}'),
+                Text('Rating: ${item.rating}'),
+              ],
+            ),
+            ),
+        );
+    }
 }
