3c226f9101ab2b2dc7cb70fcf10a96d02b20a5e5
dimasprmana <dimasprmana>
2024-10-07 10:41:14 +0700

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../models/item.dart'; 
+class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Nama barang'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, '/item', arguments: item);
+            },
+            child: Card(
+              child: ListTile(
+                title: Text(item.name),
+                subtitle: Text('Price: \$${item.price}'),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
